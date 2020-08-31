c27d8c7a8dba8ede1bab335380f24bf53f16e2b6
Burmy <62005549+Burmy@users.noreply.github.com>
2020-08-30 18:29:20 -0700

    fixing database errors

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true,
+        ssl: {
+
+            rejectUnauthorized: false
+
+        }
 
     }
 });
