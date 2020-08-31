0b8977883f26d3862e4345c97b180eec9080d30d
Burmy <62005549+Burmy@users.noreply.github.com>
2020-08-30 18:06:32 -0700

    updating database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-metric-96579',
         user: 'burmy',
         password: '',
         database: 'face-ai'
